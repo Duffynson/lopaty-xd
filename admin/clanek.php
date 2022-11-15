cedbd9a0d49a726da7da377b292f7d7f8a7b0f02
Dufy <96007248+Duffynson@users.noreply.github.com>
2022-11-15 19:10:47 +0100

    Update clanek.php

--- admin/clanek.php
+++ admin/clanek.php
@@ -10,7 +10,7 @@ include_once './php/components/header.php';
 ?>
 
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Články</h1>
+        <h1 class="h2 justify-content-center">Přidání článku</h1>
       </div>
       
       <div class="d-flex justify-content-center">
