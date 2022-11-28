9f6aac527cf876254022fcb593e0688037708676
Dufy <96007248+Duffynson@users.noreply.github.com>
2022-11-28 22:40:33 +0100

    Update add_article.js

--- admin/js/add_article.js
+++ admin/js/add_article.js
@@ -10,7 +10,7 @@ document.querySelector('.add-article').addEventListener('submit', async (e) => {
         showAlert(responseJSON[keys[0]], keys, 0)
         e.target.reset();
     } catch(e) {
-        showAlert('Nastala chyba při ukládání článku', 'danger', 0)
+        showAlert('Nastala chyba při ukládání článku.', 'danger', 0)
     }
 
 })
