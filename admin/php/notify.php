cb9f6c1b3dc32f78bddbec0ebe8a2f87b924d8a9
Mrthaa <115627101+Mrthaa@users.noreply.github.com>
2022-11-30 13:32:04 +0100

    Update notify.php

--- admin/php/notify.php
+++ admin/php/notify.php
@@ -3,10 +3,9 @@ session_start();
 require_once '../../php/db.php';
 function add_notification($comment,$subject,$ID_user){
 
-   $notify = "INSERT INTO notifications(subject, comment,ID_user) VALUES('$subject', '$comment','$ID_user'";
+   $notify = "INSERT INTO notifications(subject, comment,ID_user) VALUES('{$subject}', '{$comment}',{$ID_user}";
    $result = mysqli_query($conn,$notify);
-   
+   }
 
-
-}
+add_notification('subject', 'bylo zapsáno', 3)
 ?>
