0e842886259c584935674ffcbba1420dfa54708d
Vojtech Cizek <cizek.vojtech@gmail.com>
2022-11-30 20:53:45 +0100

    ultra mega popici fix

--- admin/php/submit_review.php
+++ admin/php/submit_review.php
@@ -14,13 +14,14 @@ if (!(empty($aktualnost) || empty($originalita) || empty($odbornost) || empty($j
   if($check['recenzent1'] != $_SESSION['id_user'] && $check['recenzent2'] != $_SESSION['id_user']) exit(http_response_code(403));
 
   mysqli_query($conn, "UPDATE Recenze SET aktualnost = '{$aktualnost}', originalita = '{$originalita}', jazyk = '{$jazyk}', odbornost = '{$odbornost}', comment = '{$comment}', datum_recenze = '{$date}' WHERE ID_recenze = '{$_POST["id_review"]}'");
-  
-
-  $result = mysqli_query($conn, "SELECT datum_recenze FROM Recenze WHERE ID_recenze = {$_POST['id_review']}");
-  $result2 = mysqli_query($conn, "SELECT datum_recenze FROM Recenze WHERE ID_recenze = {$_POST['id_review']}");
+    
+  $res = mysqli_query($conn, "SELECT recenze1, recenze2 FROM Rizeni WHERE ID_rizeni = {$_POST['ID_rizeni']}");
+  $res = mysqli_fetch_assoc($res);
+  $result = mysqli_query($conn, "SELECT datum_recenze FROM Recenze WHERE ID_recenze = {$res['recenze1']}");
+  $result2 = mysqli_query($conn, "SELECT datum_recenze FROM Recenze WHERE ID_recenze = {$res['recenze2']}");
   $data = mysqli_fetch_assoc($result);
   $data2 = mysqli_fetch_assoc($result2);
-  if($data['datum_recenze'] != null && $data2['datum_recenze'] != null) mysqli_query($conn, "UPDATE Rizeni SET status = 'REVIEWS_SUBMITTED' WHERE ID_rizeni = {$_POST['ID_rizeni']}");
+  if($data['datum_recenze'] != NULL && $data2['datum_recenze'] != NULL) mysqli_query($conn, "UPDATE Rizeni SET status = 'REVIEWS_SUBMITTED' WHERE ID_rizeni = ${$_POST['ID_rizeni']}");
   
   mysqli_close($conn);
   exit(json_encode(array("success" => "Nahrání recenze proběhlo úspěšně, za okamžik proběhne přesměrování.")));
@@ -30,4 +31,4 @@ else{
 }
 mysqli_close($conn);
 http_response_code(500);
-?>
+?> 
