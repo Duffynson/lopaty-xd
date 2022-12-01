a9269c4e8f9df216e1adb4811bbd3623e7014aee
japko <cizek.vojtech@seznam.cz>
2022-12-01 11:35:16 +0100

    Update request_review.php

--- admin/php/request_review.php
+++ admin/php/request_review.php
@@ -10,7 +10,7 @@ if(!isset($_SESSION['id_user'])) {
 
 if(!isset($_GET['id'])) exit(http_response_code(500));
 
-if($stmt = $conn->prepare("SELECT Article.ID_user as ID_autor, ID_rizeni, ID_redaktor, originalita, aktualnost, jazyk, odbornost, comment, recenzent, recenzent2, datum_recenze FROM Recenze JOIN Rizeni ON Rizeni.recenze1 = ID_recenze OR Rizeni.recenze2 = ID_recenze JOIN Article ON Rizeni.ID_article = Article.ID_article WHERE ID_recenze = {$_GET['id']}")) {
+if($stmt = $conn->prepare("SELECT Article.ID_user as ID_autor, ID_rizeni, ID_redaktor, originalita, aktualnost, jazyk, odbornost, comment, recenzent, recenzent2, recenzent1, datum_recenze FROM Recenze JOIN Rizeni ON Rizeni.recenze1 = ID_recenze OR Rizeni.recenze2 = ID_recenze JOIN Article ON Rizeni.ID_article = Article.ID_article WHERE ID_recenze = {$_GET['id']}")) {
     $stmt->execute();
     $result = $stmt->get_result();
     $data = $result->fetch_all(MYSQLI_ASSOC);
