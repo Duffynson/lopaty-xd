4f222347d90bab3f97b5ca11bbebbc5647ec0a69
Fojta <115902797+Fojta@users.noreply.github.com>
2022-12-18 02:17:46 +0100

    Add files via upload

---
+++ admin/data_archiving.php
@@ -0,0 +1,51 @@
+<?php
+$DB_HOST = "34.116.235.60";
+$DB_USER = "rsp_user";
+$DB_PASS = "uOb3jlOI";
+$DB_NAME = "rsp";
+$con = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+ 
+ $tables = array();
+
+$result = mysqli_query($con,"SHOW TABLES");
+while ($row = mysqli_fetch_row($result)) {
+    $tables[] = $row[0];
+}
+
+$return = '';
+
+foreach ($tables as $table) {
+    $result = mysqli_query($con, "SELECT * FROM ".$table);
+    $num_fields = mysqli_num_fields($result);
+
+    $return .= 'DROP TABLE '.$table.';';
+    $row2 = mysqli_fetch_row(mysqli_query($con, 'SHOW CREATE TABLE '.$table));
+    $return .= "\n\n".$row2[1].";\n\n";
+
+    for ($i=0; $i < $num_fields; $i++) { 
+        while ($row = mysqli_fetch_row($result)) {
+            $return .= 'INSERT INTO '.$table.' VALUES(';
+            for ($j=0; $j < $num_fields; $j++) { 
+                $row[$j] = addslashes($row[$j]);
+                if (isset($row[$j])) {
+                    $return .= '"'.$row[$j].'"';} else { $return .= '""';}
+                    if($j<$num_fields-1){ $return .= ','; }
+                }
+                $return .= ");\n";
+            }
+        }
+        $return .= "\n\n\n";
+    
+}
+                 ;
+
+
+$handle = fopen('archieved_data.sql', 'w+');
+fwrite($handle, $return);
+fclose($handle);
+echo "<script>
+             alert('Archivace dat proběhla úspěšně!'); 
+             window.history.go(-1);
+     </script>";
+
+?>
