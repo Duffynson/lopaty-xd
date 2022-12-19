61c788d5754baca66942a212562e47ec37f8ba55
japko <cizek.vojtech@seznam.cz>
2022-12-19 19:25:29 +0100

    Update header.php

--- admin/php/components/header.php
+++ admin/php/components/header.php
@@ -79,6 +79,13 @@
               Podpora
             </a>
           </li>" ?>
+        <?php if(isset($_SESSION['id_user']) && $_SESSION['role'] == 4) echo "
+          <li class='nav-item'>
+            <a class='nav-link' aria-current='page' href='./php/backup'>
+              <i class='fa-solid fa-users fa-fw'></i>
+              Zálohovat data
+            </a>
+          </li>" ?>;
         </ul>
 
           <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
